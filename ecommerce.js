bf6cb5d0da3f8b07c51119eb1c0956c3e6387626
Shamza <sharleenjamoza@gmail.com>
2024-02-04 22:06:46 +0300

    restored previous modifications

--- ecommerce.js
+++ ecommerce.js
@@ -1,3 +1,5 @@
 const header = document.querySelector('header');
 
-window.addEventListener('scroll', function)
+window.addEventListener('scroll', function(){ //we've selected the whole window object
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+})
